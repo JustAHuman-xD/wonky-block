cf258732c3777dd5a47c362c41807f345dd0a724
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-10-24 15:02:33 -0500

    Its a wip

--- src/main/java/nl/enjarai/wonkyblock/compat/sodium/mixin/ChunkRenderRebuildTaskMixin.java
+++ src/main/java/nl/enjarai/wonkyblock/compat/sodium/mixin/ChunkRenderRebuildTaskMixin.java
@@ -1,17 +1,25 @@
 package nl.enjarai.wonkyblock.compat.sodium.mixin;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
-import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.tasks.ChunkBuilderMeshingTask;
-import me.jellysquid.mods.sodium.client.util.task.CancellationToken;
-import org.spongepowered.asm.mixin.Dynamic;
+import nl.enjarai.wonkyblock.WonkyBlock;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Pseudo
 @Mixin(ChunkBuilderMeshingTask.class)
 public abstract class ChunkRenderRebuildTaskMixin {
-
+    @WrapWithCondition(
+            method = "execute",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderer;renderModel(Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderContext;Lme/jellysquid/mods/sodium/client/render/chunk/compile/ChunkBuildBuffers;)V"
+            ),
+            remap = false
+    )
+    private boolean wonkyblock$hideBlock(BlockRenderer renderer, BlockRenderContext context, ChunkBuildBuffers buffers) {
+        return !WonkyBlock.getInvisibleBlocks().contains(context.pos());
+    }
 }
